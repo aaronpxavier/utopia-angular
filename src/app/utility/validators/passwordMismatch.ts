40459ffdbcc30e772befbdab25996aa1312d2222
Arun Mavumkal <av8r88@gmail.com>
2021-01-16 18:54:58 -0500

    modified passwordMismatch.ts validator removed extra unnessary check in if statement

--- src/app/utility/validators/passwordMismatch.ts
+++ src/app/utility/validators/passwordMismatch.ts
@@ -4,7 +4,7 @@ export const passwordMismatch = (abstractControl: AbstractControl): void => {
   const password = abstractControl.get('password');
   const confirmPassword = abstractControl.get('confirmPassword');
 
-  if (password.value && password.value && password.value === confirmPassword.value) {
+  if (password.value && password.value === confirmPassword.value) {
     confirmPassword.setErrors(null);
   } else if (password.value) {
     confirmPassword.setErrors({...confirmPassword.errors, passwordMismatch: true});
