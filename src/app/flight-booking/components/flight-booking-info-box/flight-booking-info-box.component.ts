daa44225e307abd0f5b5cc141cf279ea46f8b7d7
Arun Mavumkal <av8r88@gmail.com>
2021-01-25 06:55:34 -0500

    ut-60 sort feature is complete

---
+++ src/app/flight-booking/components/flight-booking-info-box/flight-booking-info-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-booking-info-box',
+  templateUrl: './flight-booking-info-box.component.html',
+  styleUrls: ['./flight-booking-info-box.component.scss']
+})
+export class FlightBookingInfoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
