70ab10e1df69b0bba22acf6038e5169352ad45c5
Arun Mavumkal <av8r88@gmail.com>
2021-01-25 17:36:51 -0500

    removed flight duration feature

--- src/app/flight-booking/models/flight.ts
+++ src/app/flight-booking/models/flight.ts
@@ -43,21 +43,5 @@ export class Flight {
     return this.getNumLegs() > 1 ? this.getLegs()[0].price + this.getLegs()[1].price : this.getLegs()[0].price;
   }
 
-  public calculateTimeBetween(): string {
-    const legs = this.getLegs();
-    const departureTime = legs[0].departTime;
-    if (legs[0].flightNumber === '1451') {
-      console.log('1451');
-    }
-    const arrivalTime = legs.length === 2 ? legs[1].arrivalTime : legs.length === 1 ? legs[0].arrivalTime : undefined;
-    if (arrivalTime === undefined) {
-      throw Error('Arrival time undefined');
-    }
-    // const hoursMins = (arrivalTime.getMilliseconds() - departureTime.getMilliseconds() / 3600) % 24);
-    const seconds = (arrivalTime.getTime() - departureTime.getTime());
-    const hours = Math.floor(seconds / 3600000);
-    const min = Math.floor(seconds / 3600000 % 1 * 60);
-    return hours + 'h ' +  (min ? min + 'm' : '');
-  }
 }
 
