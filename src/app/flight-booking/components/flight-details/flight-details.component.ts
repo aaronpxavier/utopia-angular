81d9f0dad03b4530bb2701567c7452e2886b04aa
Ron Henson <ron.henson@gmail.com>
2021-01-29 08:59:46 -0800

    Added total price calculation

--- src/app/flight-booking/components/flight-details/flight-details.component.ts
+++ src/app/flight-booking/components/flight-details/flight-details.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AirportModel } from 'src/app/shared/models/types';
 import { Flight, FlightModel } from '../../models/flight';
+import {TripType} from '../../models/types';
 
 @Component({
   selector: 'app-flight-details',
@@ -10,12 +11,21 @@ import { Flight, FlightModel } from '../../models/flight';
 })
 export class FlightDetailsComponent implements OnInit {
 
-  @Input() departAirport: AirportModel;
-  @Input() arriveAirport: AirportModel;
+  @Input() departingFlight: Flight;
+  @Input() returnFlight: Flight;
+  @Input() tripType: TripType;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: FlightModel[]) {
    }
 
   ngOnInit(): void {
+  }
+
+  calculatePrice(): number {
+    let sum = 0;
+    for  (const flightLeg of this.data) {
+      sum += flightLeg.price;
+  }
+    return sum;
   }
 }
